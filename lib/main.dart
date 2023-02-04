33073c5799d969915360adefd9897d6c821a00d1
Cason Pittman <xhiquality@gmail.com>
2023-02-04 15:09:15 -0500

    WorkingNavBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:vaughn_s_application6/presentation/home_pg_screen/home_pg_screen.dart';
+import 'package:vaughn_s_application6/presentation/search_page_screen/search_page_screen.dart';
 import 'package:vaughn_s_application6/widgets/navbar.dart';
 
 void main() {
@@ -17,6 +18,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,8 +29,35 @@ class _MyAppState extends State<MyApp> {
       ),
       title: 'vaughn_s_application6',
       home: Scaffold(
-        body: HomePgScreen(),
-        bottomNavigationBar: NavBarCustom(),
+        body: Center(
+            child: currentIndex == 0
+                ? Container(
+                    child: HomePgScreen(),
+                  )
+                : Container(
+                    child: SearchPageScreen(),
+                  )),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+                label: 'Home',
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.black,
+                )),
+            BottomNavigationBarItem(
+              label: 'Search',
+              icon: Icon(Icons.search_sharp, color: Colors.black),
+            ),
+          ],
+          currentIndex: currentIndex,
+          fixedColor: Colors.teal,
+          onTap: (int index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        ),
       ),
     );
   }
--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
 
-class NavBarCustom extends StatelessWidget {
+class NavBarCustom extends StatefulWidget {
+  @override
+  State<NavBarCustom> createState() => _NavBarCustomState();
+}
+
+class _NavBarCustomState extends State<NavBarCustom> {
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(items: const [
-      BottomNavigationBarItem(
-          label: 'Home',
-          icon: Icon(
-            Icons.home,
-          )),
-      BottomNavigationBarItem(
-        label: 'Search',
-        icon: Icon(Icons.search_sharp),
-      ),
-      BottomNavigationBarItem(
-          label: 'Emergency', icon: Icon(Icons.emergency_rounded))
-    ]);
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(
+              Icons.home,
+              color: Colors.teal,
+            )),
+        BottomNavigationBarItem(
+          label: 'Search',
+          icon: Icon(Icons.search_sharp, color: Colors.teal),
+        ),
+      ],
+      currentIndex: currentIndex,
+      onTap: (int index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+    );
   }
 }
