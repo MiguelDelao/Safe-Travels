6c01de84e4add7bb5b6b4b8239dd2bda7334b53e
Miguel Delao <miguel@delao.org>
2023-02-04 13:39:18 -0500

    Update logic.dart

--- lib/logic.dart
+++ lib/logic.dart
@@ -3,19 +3,21 @@ import 'package:flutter/services.dart';
 import 'travelAdvisory.dart';
 import 'package:http/http.dart' as http;
 
-Future<Map<String, dynamic>> loadJson() async {
+Future<Map<String, dynamic>> loadCountries() async {
   String jsonString = await rootBundle.loadString('/countries.json');
   Map<String, dynamic> countries = json.decode(jsonString);
   return countries;
 }
 
-void fetchInfo(String code) async {
+Future<Map<String, dynamic>> fetchCountryAdvisory(String code) async {
   Uri uu = Uri.parse("https://www.travel-advisory.info/api?countrycode=$code");
   final response = await http.get(uu);
   print(response.body);
 
-  Map<String, dynamic> possible = json.decode(response.body);
-  print(possible["data"][code]["advisory"]["score"]);
+  Map<String, dynamic> info = json.decode(response.body);
+  info["data"][code]["advisory"]["score"];
+
+  return info;
 }
 
 void testApi() async {}
