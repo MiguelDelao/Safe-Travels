f8807d40042793d57c1d6f4ab94fb69747c660c2
sean-michael-mlvt <smm65270@uga.edu>
2023-02-04 23:13:34 -0500

    corrected language variable
    changed the language variable to correctly pull the language instead of the demonym for a given country.

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\BowOf\\OneDrive\\Documents\\FlutterDev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\BowOf\\StudioProjects\\UGAHacks8\\zeus" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\BowOf\\OneDrive\\Documents\\FlutterDev\\flutter"
+  "PROJECT_DIR=C:\\Users\\BowOf\\StudioProjects\\UGAHacks8\\zeus"
+  "FLUTTER_ROOT=C:\\Users\\BowOf\\OneDrive\\Documents\\FlutterDev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\BowOf\\StudioProjects\\UGAHacks8\\zeus\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\BowOf\\StudioProjects\\UGAHacks8\\zeus"
+  "FLUTTER_TARGET=C:\\Users\\BowOf\\StudioProjects\\UGAHacks8\\zeus\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\BowOf\\StudioProjects\\UGAHacks8\\zeus\\.dart_tool\\package_config.json"
+)
