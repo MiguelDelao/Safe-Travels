0cc300e1486185e27f0bdfd4c6edab7d7c1d37fa
sean-michael-mlvt <smm65270@uga.edu>
2023-02-05 01:37:34 -0500

    Added Flag Card
    Added flag card template to a temporary home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:zeus/logic.dart';
 import 'package:zeus/presentation/home_pg_screen/home_pg_screen.dart';
@@ -28,7 +30,7 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         body: Center(
             child: currentIndex == 0
-                ? Container(child: HomePgScreen())
+                ? Container(child: Cardddd())
                 : Container(
                     child: SearchPageScreen(),
                   )),
@@ -54,6 +56,63 @@ class _MyAppState extends State<MyApp> {
           },
         ),
         resizeToAvoidBottomInset: false,
+      ),
+    );
+  }
+}
+
+class Cardddd extends StatefulWidget {
+  const Cardddd({super.key});
+
+  @override
+  State<Cardddd> createState() => _CarddddState();
+}
+
+class _CarddddState extends State<Cardddd> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      padding: EdgeInsets.all(5.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.black.withOpacity(.8),
+      ),
+      height: 100,
+      width: double.infinity,
+      child: Stack(
+        children: [
+          Container(
+              height: double.infinity,
+              width: double.infinity,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: ImageFiltered(
+                  imageFilter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                  child: Image.network(
+                    'https://countryflagsapi.com/png/brazil',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )),
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: Center(
+              child: Container(
+                alignment: Alignment.center,
+                child: Text(
+                  'Brazil',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
